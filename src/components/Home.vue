fa85ef455fec4db7b217a39680b39ee5db7d9f5f
Lushenggang <lushg-tcxg@qq.com>
2018-05-27 19:04:59 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page">
+    <label>请输入要打开的pdf地址:</label>
+    <input type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
